2c0d530778d555cefc59bca79482be9fa56c43b6
Jeeems10 <jamesponce10@outlook.com>
2023-06-25 19:42:36 +0200

    qutoesAPI aber noch nicht fertig

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -7,3 +7,27 @@ let days = document.getElementsByClassName("day");
 
 days[currentDay].classList.add("border-2", "border-amber-500");
 
+
+
+
+
+const quoteText = document.querySelector(".quote");
+const authorName = document.querySelector(".author .name");
+
+const quoteBtn = document.querySelector("button");
+
+// random quote function
+function randomQuote(){
+  fetch("https://api.quotable.io/random")
+  .then(res => res.json())
+  .then(result =>{
+    console.log(result);
+    quoteText.innerText = result.content;
+    authorName.innerText = result.author;
+  })
+}
+
+quoteBtn.addEventListener("click", randomQuote);
+
+
+
