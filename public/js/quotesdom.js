9dad50ecb4f16bb8127339052861dec11a97dc75
Jeeems10 <jamesponce10@outlook.com>
2023-06-27 22:32:54 +0200

    text-to-speak and copy

--- public/js/quotesdom.js
+++ public/js/quotesdom.js
@@ -16,6 +16,32 @@ export function createQuotes(QuotesArray, firstLoad) {
         const icon = document.createElement("i");
         const divIcon = document.createElement("div")
 
+        //implementieren text-to-speak
+        const speakButton = document.createElement("button");
+        speakButton.classList.add("rounded-full", "p-2", "bg-blue-500", "text-white", "mb-4");
+        
+        const speakIcon = document.createElement("i");
+        speakIcon.classList.add("fas", "fa-volume-up");
+        
+        speakButton.appendChild(speakIcon);
+        speakButton.addEventListener("click", () => {
+          speakText(quote.quote); // Vorlesen des Zitats
+        });
+
+        //copy btn
+        const copyButton = document.createElement("button");
+        copyButton.classList.add("rounded-full", "p-2", "bg-blue-500", "text-white", "mb-4", "ml-4"); // ml-4 for left margin
+
+        const copyIcon = document.createElement("i");
+        copyIcon.classList.add("fas", "fa-copy");
+
+        copyButton.appendChild(copyIcon);
+        
+        copyButton.addEventListener("click", () => {
+          copyToClipboard(quote.quote);
+        });
+       
+
         divIcon.classList.add("flex", "justify-end","pr-4", "pb-4")
         icon.classList.add("fa", "fa-circle-plus", "text-4xl", "text-cyan-200/50", "hover:text-cyan-700/50"   );
 
@@ -38,6 +64,9 @@ export function createQuotes(QuotesArray, firstLoad) {
         article.append(author);
         divIcon.append(icon);
         article.append(divIcon);
+        article.append(speakButton);// hier append
+        article.append(copyButton);
+
 
         icon.addEventListener("click",()=>{
           makePostRequest(quote)
@@ -58,6 +87,24 @@ export function alertSuccesfull() {
   setTimeout(() => {
     alert.remove();
   }, 2000);
+}
+
+// quote text-tospeak func
+function speakText(text) {
+  const speechSynthesis = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = "en-US";
+  speechSynthesis.speak(utterance);
+}
+
+//copy button func
+function copyToClipboard(text) {
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
 }
 
 /* export function alertDanger() {
